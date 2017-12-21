dd90da3c31df3ad1f5337bd9d15c371b19d09568
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-21 10:19:55 -0800

    Add files via upload

---
+++ src/main/java/com/app/test/SaveTest.java
@@ -0,0 +1,24 @@
+package com.app.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class SaveTest {
+
+	public static void main(String[] args) {
+		//read and print the employee i.e parent object
+				ApplicationContext ctx=new ClassPathXmlApplicationContext("Springconfig.xml");
+				
+				JdbcTemplate jt=(JdbcTemplate)ctx.getBean("jtObj");
+				
+				String sql="INSERT INTO EMPLOYEE VALUES(?,?,?)";
+				
+				int count=jt.update(sql, 11,"Rohit",14.36f);
+				
+				System.out.println(count+" Rows inserted");
+				
+
+	}
+
+}
